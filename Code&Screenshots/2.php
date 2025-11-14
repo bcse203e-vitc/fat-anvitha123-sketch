15ab3825d9fb0a5cc2843d1f231e02b859df0f46
anvitha123-sketch <chitmal.anvitha000@gmail.com>
2025-11-14 15:45:53 +0530

    Add files via upload

--- Code&Screenshots/2.php
+++ Code&Screenshots/2.php
@@ -1,25 +1,44 @@
 <?php
-function insideOut($arr) {
-    $length = count($arr);
 
-  
-    if ($length % 2 !== 0) {
-        return $arr; 
+function insideOut(array $arr): array
+{
+    $count = count($arr);
+
+    // Only transform if the array has an even number of elements
+    if ($count % 2 !== 0) {
+        return $arr;
     }
 
-  
-    $half = $length / 2;
-
-    
+    $half = $count / 2;
     $result = $arr;
 
+    // Swap outermost elements moving inward
     for ($i = 0; $i < $half; $i++) {
-       
-        $result[$i] = $arr[$i + $half];
-        $result[$i + $half] = $arr[$i];
+        $j = $count - 1 - $i;
+        $temp = $result[$i];
+        $result[$i] = $result[$j];
+        $result[$j] = $temp;
     }
 
     return $result;
 }
+
+
+$numericExample = [1, 2, 2, 1];
+$numericResult  = insideOut($numericExample);
+
+
+$stringExample = ["Everyone", "says", "Kelly", "is", "REALLY", "awesome"];
+$stringResult  = insideOut($stringExample);
+
+
+
+echo "Numeric Example:\n";
+print_r($numericResult);
+
+echo "\nString Example:\n";
+print_r($stringResult);
+
 ?>
+
 
